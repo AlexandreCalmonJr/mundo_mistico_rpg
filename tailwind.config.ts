951f5b07d72310318b43bfe726a73ce4c67d5792
Alexandre Calmon <Alexandrecalmonjunior@gmail.com>
2025-09-29 15:30:52 +0000

    Sistema de criação de personagens com classes e raças Sistema de aventur

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,10 +88,25 @@ export default {
             height: '0',
           },
         },
+        'fade-in-down': {
+          from: { opacity: '0', transform: 'translateY(-10px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+        'fade-in-up': {
+          from: { opacity: '0', transform: 'translateY(10px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+         'fade-in-left': {
+          from: { opacity: '0', transform: 'translateX(-20px)' },
+          to: { opacity: '1', transform: 'translateX(0)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'fade-in-down': 'fade-in-down 0.5s ease-out forwards',
+        'fade-in-up': 'fade-in-up 0.5s ease-out 0.2s forwards',
+        'fade-in-left': 'fade-in-left 0.5s ease-out forwards',
       },
     },
   },
